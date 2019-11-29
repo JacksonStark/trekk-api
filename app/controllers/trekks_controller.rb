dc6b587f310ea96e8a83b4d7d79cf6109107ac0e
Jackson Stark <jacksonstark77@hotmail.com>
2019-11-28 17:55:51 -0800

    fix marker json response error

--- app/controllers/trekks_controller.rb
+++ app/controllers/trekks_controller.rb
@@ -2,9 +2,18 @@ class TrekksController < ApplicationController
 
   def create
     trekk = Trekk.new(trekk_params)
-    trekk.access_code = SecureRandom.hex(3)
+    access_code = SecureRandom.hex(3)
+    trekk.access_code = access_code
     trekk.save
+    created_trekk = Trekk.find_by(access_code: access_code)
+
+    render :json => { trekk_id: created_trekk.id }
   end
+
+  # def update 
+  #   trekk = Trekk.find_by(trekk_id: params[:trekk_id])
+  #   trekk.update(name: params[:name])
+  # end
 
   def show
     trekks = Trekk.where(user_id: params[:user_id])
@@ -20,7 +29,7 @@ class TrekksController < ApplicationController
   private
 
   def trekk_params
-    params.permit(:name, :user_id)
+    params.permit(:user_id, :name)
   end
 
 end
