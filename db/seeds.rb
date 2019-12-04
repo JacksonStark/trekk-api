521a877381721a7be92e066ada76b2d67429a9ae
Jackson Stark <jacksonstark77@hotmail.com>
2019-12-03 18:19:33 -0800

    changed marker image patterns

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,42 +44,42 @@ user1.trekks.create(
 trekk1 = Trekk.find_by(name: 'Lighthouse Tour')
 
 trekk1.markers.create(
-  marker_image: 'https://i.ibb.co/nfj56Jk/trekk-Marker1.png',
+  marker_image: 'https://i.ibb.co/QkXpp99/target-Photo1.png',
   spawned_description: "Have some fun in the Rainbow Room, where you'll often find project teams planning and presenting.",
   spawned_image: 'https://www.journeymexico.com/project-team?showimg_aldggj=project+team.jpg',
   spawned_video: ''
 )
 
 trekk1.markers.create(
-  marker_image: 'https://i.ibb.co/y5xRWxL/trekk-Marker2.png',
+  marker_image: 'https://i.ibb.co/M5zcctr/target-Photo2.png',
   spawned_description: 'Welcome to our lounge, where students and alumni come to hit pause on their busy lives and take it easy.',
   spawned_image: "http://static.pokemonpets.com/images/monsters-images-800-800/81-Magnemite.png",
   spawned_video: ''
 )
 
 trekk1.markers.create(
-  marker_image: 'https://i.ibb.co/64Yw6tC/trekk-Marker3.png',
+  marker_image: 'https://i.ibb.co/x3Yzqt1/target-Photo3.png',
   spawned_description: "This is our coffee machine. Arguably the heart of Lighthouse Labs. Don't be shy, drink some Chai.",
   spawned_image: 'https://www.betterhealthforwomen.com/wp-content/uploads/2019/02/china-coffee-cup.jpg',
   spawned_video: ''
 )
 
 trekk1.markers.create(
-  marker_image: 'https://i.ibb.co/xmxKyzT/trekk-Marker4.png',
+  marker_image: 'https://i.ibb.co/xhVK3nq/target-Photo4.png',
   spawned_description: "This is what we call... The Other Side. It is leased by other tech companies of many varieties.",
   spawned_image: 'https://wheretogoin.net/wp-content/uploads/2019/04/there-are-two-sides-to-every-story-e1554505904383.jpg',
   spawned_video: ''
 )
 
 trekk1.markers.create(
-  marker_image: 'https://i.ibb.co/80zPKms/trekk-Marker5.png',
+  marker_image: 'https://i.ibb.co/g4LcX4g/target-Photo5.png',
   spawned_description: "If you're unsure how to use these modernized elevators, don't worry! They're smarter than you think!",
   spawned_image: 'https://geekologie.com/2015/08/10/personal-home-elevat0r.jpg',
   spawned_video: ''
 )
 
 trekk1.markers.create(
-  marker_image: 'https://i.ibb.co/txyMvg3/trekk-Marker6.png',
+  marker_image: 'https://i.ibb.co/09PbqDg/target-Photo6.png',
   spawned_description: 'Here we have Nima, lover of Kanye and turtlenecks',
   spawned_image: 'https://images.glaciermedia.ca/polopoly_fs/1.416929.1374534817!/fileImage/httpImage/image.jpg_gen/derivatives/landscape_804/img-0-6264716-jpg.jpg',
   spawned_video: ''
